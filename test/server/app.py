9f25e01a0ae52fbddacc233170a7b5987a5aa45c
Yasyf Mohamedali <yasyfm@gmail.com>
2017-09-18 13:39:39 -0700

    Always add new comments, never edit.

--- test/server/app.py
+++ test/server/app.py
@@ -18,29 +18,32 @@ def full_pr_message(valid, message):
   if valid:
     return 'Your application is valid! :sunglasses:'
   else:
-    return 'Your application is invalid! **{}**'.format(message)
+    return 'Your application is invalid! The reason is: **{}**'.format(message)
 
 def create_issue_comment(repo, user):
   title = "Invalid Application: {}".format(user)
   valid_message = 'Thanks @{}, your application has been fixed!'.format(user)
-  invalid_message = 'Hey @{}, your application is invalid! **{}**'.format(user, request.values['message'])
+  invalid_message = 'Hey @{}, {}'.format(user, full_pr_message(False, request.values['message']))
   try:
-    issue = repo.get_issues(assignee=user)[0]
+    issue = repo.get_issues(mentioned=github.get_user(user))[0]
   except IndexError:
     if not valid():
-      repo.create_issue(title, invalid_message, user, labels=['application-invalid'])
+      repo.create_issue(title, invalid_message, labels=['application-invalid'])
   else:
     if valid():
       issue.create_comment(valid_message)
       issue.edit(state='closed')
     else:
-      issue.create_comment(invalid_message)
+      comments = list(issue.get_comments())
+      body = comments[-1].body if comments else issue.body
+      if body != invalid_message:
+        issue.create_comment(invalid_message)
 
 def create_pr_comment(pr):
-  comment = next(filter(lambda x: x.user.login == os.getenv('GH_USER'), pr.get_issue_comments()), None)
   message = full_pr_message(valid(), request.values.get('message'))
-  if comment:
-    comment.edit(message)
+  comments = list(filter(lambda x: x.user.login == os.getenv('GH_USER'), pr.get_issue_comments()))
+  if comments and comments[-1].body == message:
+    return
   else:
     pr.create_issue_comment(message)
 
